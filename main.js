590ddac41ea3f9b6b007f22fb03b47f507c974cf
cmoc2 <cmortiz@ucsc.edu>
2015-02-26 11:19:29 -0800

    asdas

--- main.js
+++ main.js
@@ -180,6 +180,7 @@ function toggleGame(){
 			}
 }
 //added.
+//somethignafter
 function menu(){//hi cmoc2 blahblah bloop
 	ctx.drawImage(main_background,0,0, canvas.width,canvas.height);
 	ctx.drawImage(play_button, canvas.width/2-50, canvas.height/2);
@@ -189,7 +190,7 @@ function menu(){//hi cmoc2 blahblah bloop
 	//tothis
 	// yeah.
 }
-
+//charmanders
 function settingsPage(){
 	ctx.drawImage(settings_background, 0, 0, canvas.width, canvas.height);
 	ctx.strokeText("Setting stuff goes here", canvas.width/2, canvas.height/2 - 15);
