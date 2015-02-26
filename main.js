f4663505e23760642c22eb10079e0c852ab01ffc
cmoc2 <cmortiz@ucsc.edu>
2015-02-26 10:51:22 -0800

    Added 2 values

--- main.js
+++ main.js
@@ -181,7 +181,7 @@ function toggleGame(){
 }
 
 function menu(){
-	ctx.drawImage(main_background,0,0);
+	ctx.drawImage(main_background,0,0, canvas.width,canvas.height);
 	ctx.drawImage(play_button, canvas.width/2-50, canvas.height/2);
 	ctx.drawImage(settings_button, canvas.width-50, canvas.height-50, 50, 50);
 	ctx.drawImage(how_to_play_button, 0, canvas.height-31, 235, 31);
