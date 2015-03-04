93766e35b0c224ce7a8fe42e1546c920806da488
cmoc2 <cmortiz@ucsc.edu>
2015-03-04 06:32:49 -0800

    main.js: -removed play_button from top(unneccesary). -added 4 new buttons -added a pause menu (removed no_state) 	pause menu options: 	+return to game 	+music toggle 	+sound toggle 	+quit game -removed toggleGame & its variable. -added soundFX bool to control click sounds. -used vars to make it easier to detect if a button was clicked. -added back & music button to settings -added back button to (temporary?) tutorial. -added pause button to game screen. -space bar action easier to understand.

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
-var settings, play_music, play_button, main_background, evt, 
+var settings, play_music, main_background, evt,
 //sounds
 track, button_sound,
 //objects
 canvas, ctx, _screen, mousePos = {x: 0, y: 0}, walker,
 //images:
+back_button_image = "back_button.png",
+cyan_button_image = "cyan_button.png",
+dark_button_image = "dark_button.png",
 	/*menu*/
 play_button_image = "play_button.gif",
 how_to_play_image = "how_to_play_button.jpg", main_background_image = "main_background.jpg",
 
 	/*game stuff*/
 game_background_image = "game_bkgrnd.jpg",
+pause_button_image = "pause_button.png",
 	/*settings*/
 settings_image = "Gear_Icon.png",settings_background_image = "tsunami.jpg",
 //Numbers correspond to _screen
 main_menu = 1, settings = 2, play_game = 3, how_to_play = 4, 
-pause_menu = 5, no_state = null;
+pause_menu = 5,
 //KeyCodes
 Key_Space = 32,
 Key_1 = 49, Key_2 = 50, Key_3 = 51, Key_4 = 52,
 Key_M = 77, Key_K = 75,
 
 //bools
-musicOn = true; pausedGame = false;
+musicOn = true, soundFX = true;
 
 //city nodes
 var LA = {key: "LA", population: 100000, next: null, HP: 100, posX: 100, posY: 100, color: '#CC0000'};
@@ -69,23 +73,42 @@ function clickLocation(evt){
 		case main_menu:
 			initiateInGameCityList();
 			//if on play button
-			if( mousePos.x >=360 && mousePos.x<=490 && mousePos.y >= 310 && mousePos.y <= 480){
+			if( mousePos.x >=canvas.width/2-50 && mousePos.x<=canvas.width/2-50 + play_button.width
+				 && mousePos.y >= canvas.height/2 && mousePos.y <= canvas.height/2 + play_button.height){
 			init();
-			button_sound.play();
+			if(soundFX == true) button_sound.play();
 			_screen = play_game;
 			}	//if on gear icon
 			else if(mousePos.x>=canvas.width-50 && mousePos.x<=canvas.width
 					 && mousePos.y>=canvas.height-50 && mousePos.y<=canvas.height){
 						    _screen = settings;
-						    button_sound.play();
+						    if(soundFX == true) button_sound.play();
 				} //if on tutorial
 				else if(mousePos.x>=0 && mousePos.x<=231
 					&& mousePos.y>=canvas.height-31 && mousePos.y <=canvas.height){
 						_screen = how_to_play;
-						button_sound.play();
+						if(soundFX == true) button_sound.play();
 					}
 			break;
 		case settings:
+			//if on back button
+			if(mousePos.x >=0 && mousePos.x<=50 &&
+			mousePos.y >=0 && mousePos.y<=50){
+				if(soundFX == true) button_sound.play();
+				_screen = main_menu;
+				}
+			//if on music button
+			if(mousePos.x>=canvas.width/2-126 && mousePos.x<=canvas.width/2-126+cyan_button.width
+				&& mousePos.y>= canvas.height/2-35 && mousePos.y<= canvas.height/2-35+cyan_button.height){
+					if(musicOn){
+						track.load();
+						musicOn = false;
+					} else{
+						musicOn = true;
+						track.play();
+					}
+					if(soundFX == true) button_sound.play();
+			}
 			break;
 		case play_game:
 			walker = inGameCityList.head;
@@ -93,12 +116,45 @@ function clickLocation(evt){
 			{
 				if(Math.sqrt((mousePos.x - walker.posX)*(mousePos.x - walker.posX) + (mousePos.y - walker.posY)*(mousePos.y - walker.posY)) < 50){console.log(walker.key);}
 			}
+			//if on pause
+			if(mousePos.x >= canvas.width - 50 && mousePos.x <= canvas.width && mousePos.y >=0 && mousePos.y <= 50){
+				_screen = pause_menu;
+				if(soundFX == true) button_sound.play();
+			}
 			break;
 		case how_to_play:
+			//if on back button
+			if(mousePos.x >=0 && mousePos.x<=50 &&
+			mousePos.y >=0 && mousePos.y<=50){
+				if(soundFX == true) button_sound.play();
+				_screen = main_menu;
+				}
+			
 			break;
-		case no_state:
-			toggleGame();
+		case pause_menu:
+			//Return to menu
+			if(mousePos.x >= 325 && mousePos.x<=325+dark_button.width &&
+			mousePos.y >=170+40*1 && mousePos.y<=170+40*1+dark_button.height)
+				_screen = play_game;
+			//Music
+			if(mousePos.x >= 325 && mousePos.x<=325+dark_button.width &&
+			mousePos.y >=170+40*2 && mousePos.y<=170+40*2+dark_button.height){
+				if(musicOn){ track.load(); }
+					else{ track.play();}
+				musicOn=!musicOn; 
+			}
+			//Sound
+			if(mousePos.x >= 325 && mousePos.x<=325+dark_button.width &&
+			mousePos.y >=170+40*3 && mousePos.y<=170+40*3+dark_button.height)
+				soundFX = !soundFX;
+			//Quit Game
+			if(mousePos.x >= 325 && mousePos.x<=325+dark_button.width &&
+			mousePos.y >=170+40*4 && mousePos.y<=170+40*4+dark_button.height){
+				track.load(); if(musicOn) track.play(); _screen = main_menu;
+			}
+			if(soundFX == true) button_sound.play();
 			break;
+			
 		default:
 	}
 }
@@ -107,15 +163,10 @@ function keyboardAction(evt){
 	keyvalue = evt.keyCode;
 	switch(keyvalue){
 		case Key_Space:
-			//prevents game from starting when Space
-			//is pressed outside of the game page. 
-			if(_screen != no_state && _screen != play_game){
-				_screen = main_menu;
-				console.log("Action: Main_Menu");
-			} else toggleGame();
-			//prevents game from starting when Space
-			//is pressed outside of the game page. 
-			
+			//toggle menu on game. Main menu Otherwise.
+			if(_screen == pause_menu) _screen = play_game;
+				else if(_screen == play_game) _screen = pause_menu;
+					else{ _screen = main_menu; console.log("Action: Main Menu");}
 			break;
 		case Key_1:
 			console.log("Action 1:menu");
@@ -156,15 +207,16 @@ function update(){
 	//console.log(mousePos.x + ","+ mousePos.y)     //test mouse coordinates
 	if(_screen == play_game)
 	{	
-
+		
 	}
 }
 
-function draw(){ //I think this can be cleaned up a bit.
+function draw(){
 	if(_screen == main_menu) menu();
-	if(_screen == play_game) theGame();
+	if(_screen == play_game){ theGame();}
 	if(_screen == settings) settingsPage();
 	if(_screen == how_to_play) how_to_play_page();
+	if(_screen == pause_menu) pauseMenu();
 }
 
 function game_loop(){
@@ -173,13 +225,14 @@ function game_loop(){
 }
 
 function init(){
-	rectstart = 10;
+	test = 0;
 }
 //clooop[]https://github.com/lazytaroice/Gaia
 //placeholder for the start of the game.
 function theGame(){	
 	canvas.width = canvas.width;
 	ctx.drawImage(game_background, 0, 0, canvas.width, canvas.height);
+	ctx.drawImage(pause_button, canvas.width-50, 0, 50, 50);
 
 	var walker = inGameCityList.head;
 	for(var i = 0; i<inGameCityList.length; i++)
@@ -206,21 +259,8 @@ function theGame(){
 			ctx.fillRect(0, canvas.height-100, canvas.width, canvas.height); }//black box should be decorated.
 		
 		}
+		testDraw();
 }
-
-function toggleGame(){
-	//if paused, go back to game
-	if(_screen == no_state && pausedGame == true){
-				_screen = play_game;
-				pausedGame = !pausedGame;
-				console.log("Unpaused Game.");
-	} else if(_screen == play_game){ //bring up pause menu
-				console.log("Pause");
-				_screen = no_state; //switch to pause meny when there is one.
-				pausedGame = true;
-			}
-}
-
 
 function menu(){
 	ctx.drawImage(main_background,0,0, canvas.width,canvas.height);
@@ -231,7 +271,11 @@ function menu(){
 
 function settingsPage(){
 	ctx.drawImage(settings_background, 0, 0, canvas.width, canvas.height);
-	ctx.strokeText("Setting stuff goes here", canvas.width/2, canvas.height/2 - 15);
+	ctx.drawImage(back_button,0, 0, 50, 50); //top left corner
+	ctx.drawImage(cyan_button, canvas.width/2-126, canvas.height/2-35);
+	console.log(cyan_button.height);
+	if(musicOn) ctx.strokeText("Music: On ", canvas.width/2-15, canvas.height/2);
+		else ctx.strokeText("Music: Off ", canvas.width/2-15, canvas.height/2);
 }
 
 function how_to_play_page(){
@@ -240,6 +284,28 @@ function how_to_play_page(){
 	ctx.rect(0,0,canvas.width,canvas.height);
 	ctx.fill();
 	ctx.strokeText("Tutorial", canvas.width/2, canvas.height/2 - 15);
+	ctx.drawImage(back_button,0, 0, 50, 50); //top left corner
+}
+
+function pauseMenu(){	
+	var grd=ctx.createLinearGradient(300,200,500,475);
+	grd.addColorStop(0,"black");
+	grd.addColorStop("0.5","white");
+	grd.addColorStop(1,"black");
+	ctx.fillStyle=grd;
+	ctx.fillRect(310,200,180,20+40*4); 
+	//draw the options 10px by 25px off the border.
+	ctx.drawImage(dark_button, 325,170+40*1);
+	ctx.strokeText("Return to game", 325+dark_button.width/4, 170+25+40*1);
+	
+	ctx.drawImage(dark_button, 325,170+40*2);
+	ctx.strokeText("Music: "+ musicOn, 325+dark_button.width/4, 170+25+40*2);
+	
+	ctx.drawImage(dark_button, 325,170+40*3);
+	ctx.strokeText("Sound Effects: "+ soundFX, 325+dark_button.width/4, 170+25+40*3);
+	
+	ctx.drawImage(dark_button, 325,170+40*4);
+	ctx.strokeText("Quit Game", 325+dark_button.width/4, 170+25+40*4);
 }
 
 function loadImages(){
@@ -266,12 +332,25 @@ function loadImages(){
 	//load the background image of the game tab
 	game_background = new Image();
 	game_background.src = game_background_image;
+	
+	//load the back button
+	back_button = new Image();
+	back_button.src = back_button_image;
+	//load the cyan button
+	cyan_button = new Image();
+	cyan_button.src = cyan_button_image;
+	dark_button = new Image();
+	dark_button.src = dark_button_image;
+	//load the pause button
+	pause_button = new Image();
+	pause_button.src = pause_button_image;
 }
 
 function loadAudio(){
 	track = document.getElementById("gameAudio");
 	button_sound = document.getElementById("button_sound");
 }
+
 
 main();
 setInterval(game_loop, 30);
